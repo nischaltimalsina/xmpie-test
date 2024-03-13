0562ac2c04a4e94e041d964dbac78657f7f84ca7
codevibek1 <vibek@esignature.com.np>
2024-03-13 10:49:37 +0545

    set project up with lint and prettier; add sitemap and robots.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: "./src/**/*.{js,jsx,ts,tsx}",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: './src/**/*.{js,jsx,ts,tsx}',
+    theme: {
+        fontFamily: {
+            sans: ['Source Sans Pro', 'sans-serif'],
+            condensed: ['DIN Condensed', 'sans-serif']
+        },
+        extend: {}
+    },
+    plugins: []
+};
